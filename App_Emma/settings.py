2ee07734e598078f56ce616d6dbfaca52287130c
wsomasa <wsomas@gmail.com>
2024-07-01 17:24:23 +0200

    cambio en el debug

--- App_Emma/settings.py
+++ App_Emma/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-b$qj4w@(yb0g=fceqah8x@4qrpc$c+@c9=@7son2y^i1!rh=!0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = [ ]
+ALLOWED_HOSTS = ['obe.pythonanywhere.com']
 
 
 # Application definition
