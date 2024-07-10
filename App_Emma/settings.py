67e5b355f983e60f4590d8eff2f602bb8ce4bbc9
wsomasa <wsomas@gmail.com>
2024-07-10 11:20:19 +0000

    imagenes eliminiadas de cmqlc y agregada img ws1

--- App_Emma/settings.py
+++ App_Emma/settings.py
@@ -12,23 +12,18 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 import os
 from pathlib import Path
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-b$qj4w@(yb0g=fceqah8x@4qrpc$c+@c9=@7son2y^i1!rh=!0'
 
-# SECURITY WARNING: don't run with debug turned on in production!
+
 DEBUG = False
 
 ALLOWED_HOSTS = ['obe.pythonanywhere.com' ]
 
-
-# Application definition
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -73,9 +68,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'App_Emma.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -83,9 +75,6 @@ DATABASES = {
     }
 }
 
-
-# Password validation
-# https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -103,25 +92,14 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
-# Internationalization
-# https://docs.djangoproject.com/en/5.0/topics/i18n/
-
-LANGUAGE_CODE = 'es-es'
-
+LANGUAGE_CODE = 'es-ve'
 TIME_ZONE = 'America/Caracas'
-
 USE_I18N = True
-
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.0/howto/static-files/
-
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-
-#STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
 
 LOGGING = {
     'version': 1,
@@ -161,8 +139,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'wsomas@gmail.com'
-EMAIL_HOST_PASSWORD = 'efhebehekzjtsctc'  # Utiliza una contraseña de aplicación si usas Gmail
-
+EMAIL_HOST_PASSWORD = 'efhebehekzjtsctc'
 
 
 
